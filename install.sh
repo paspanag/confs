0ee5489ae7e318dfbd65151c27ce8fa608fac43d
Peter Panaguiton <p@peterp.ca>
2016-07-05 19:59:08 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,27 @@
+# git is already a given as you need to get this from github
+
+# required
+apt install sudo vim firmware-linux firmware-iwlwifi firmware-realtek wireless-tools build-essential autoconf ranger build-essential menu xbacklight
+
 usermod -G sudo peter
 
-sudo vim firmware-linux firmware-iwlwifi firmware-realtek xorg xserver-xorg-video-intel lightdm wireless-tools compton pulseaudio pavucontrol chromium rxvt-unicode menu xbacklight redshift build-essential git autoconf connman alsa-utils fonts-roboto fonts-droid mupdf mpv ranger build-essential wicd openbox
+# gui wifi
+# apt install wicd
 
-#xen-linux-system  libxen-dev xen-tools
-#m4 ocaml ocaml-native-compilers camlp4-extra opam
+# nice rice
+# apt install redshift compton fonts-roboto fonts-droid
+
+# audio
+# apt install pulseaudio pavucontrol alsa-utils
+
+# apps that need X
+# apt install chromium rxvt-unicode mupdf mpv
+
+# X and related
+# apt install xorg xserver-xorg-video-intel lightdm openbox
+
+# xen
+# apt install xen-linux-system  libxen-dev xen-tools
+
+# ocaml
+# apt install m4 ocaml ocaml-native-compilers camlp4-extra opam
