eb996a326e96cd346ff433fffc42b355d13fc220
Peter Panaguiton <p@peterp.ca>
2016-07-05 19:50:36 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 usermod -G sudo peter
 
-sudo vim firmware-linux firmware-iwlwifi firmware-realtek xorg xserver-xorg-video-intel lightdm wireless-tools compton pulseaudio pavucontrol chromium rxvt-unicode menu xbacklight redshift build-essential git autoconf connman alsa-utils fonts-roboto fonts-droid mupdf mpv ranger build-essential  wicd
+sudo vim firmware-linux firmware-iwlwifi firmware-realtek xorg xserver-xorg-video-intel lightdm wireless-tools compton pulseaudio pavucontrol chromium rxvt-unicode menu xbacklight redshift build-essential git autoconf connman alsa-utils fonts-roboto fonts-droid mupdf mpv ranger build-essential wicd openbox
 
 #xen-linux-system  libxen-dev xen-tools
 #m4 ocaml ocaml-native-compilers camlp4-extra opam
